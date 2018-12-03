025e0d7897ae077fea39ab2bf473e4886c4bbc3b
bkfwebdev <bryant.feld@gmail.com>
2018-12-02 23:32:18 -0500

    services offered reordered

--- src/components/ServicesOffered.js
+++ src/components/ServicesOffered.js
@@ -5,11 +5,7 @@ class ServicesOffered extends Component {
       
 
         <div className = "TextBox ServicesOffered">
-        
-        <h2>Search Engine Optimization</h2>
-        <p>Do you have a pre-existing web site, is that site driving customers to your business? 
-        If not, let us help you improve the search performance and interaction metrics for your web site.</p>
-        
+
         <h2>Interactive landing Pages</h2>
         <p>We will create a well designed ,vibrant, and engaging landing page that represents your brand with class 
         and promotes your business on the web.</p>
@@ -17,7 +13,11 @@ class ServicesOffered extends Component {
         <h2>Progressive Web Apps</h2>
         <p>Allow your customers to purchase your products and services on any device at any time. 
         Progressive Web Apps provide the convenience of a device specific app with the flexibility of a web page.</p>
-
+        
+        <h2>Search Engine Optimization</h2>
+        <p>Do you have a pre-existing web site, is that site driving customers to your business? 
+        If not, let us help you improve the search performance and interaction metrics for your web site.</p>
+        
         <h2>Social Media Management</h2>
         <p>With the importance of social media in today's business environment you need someone fully dedicated to representing your brand
          and engaging your customers on the various social media platforms. We can handle that for you!</p>
